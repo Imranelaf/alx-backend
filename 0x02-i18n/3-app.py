5da7e0f1b4ee487c33dadef6e08660cac828e009
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2024-07-02 23:19:19 +0100

    Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+'''
+This module creates a Flask app.
+'''
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    '''
+    This class creates a configuration object.
+    '''
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    '''
+    This function determines the best match with our supported languages.
+    '''
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def index() -> str:
+    '''
+    This function returns a string.
+    '''
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
